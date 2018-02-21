52824ee68d1479a37c31b521d774cb987c17153c
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-02-21 15:08:22 -0700

    Initial commit.

---
+++ TwitchLib.Api/ApiSection.cs
@@ -0,0 +1,12 @@
+﻿namespace TwitchLib.Api
+{
+    public class ApiSection
+    {
+        protected readonly TwitchAPI Api;
+
+        protected ApiSection(TwitchAPI api)
+        {
+            Api = api;
+        }
+    }
+}
--- TwitchLib.Api/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TwitchLib.Api
-{
-    public class Class1
-    {
-    }
-}
