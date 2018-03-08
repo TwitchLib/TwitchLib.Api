24df0597a27107483490133a256b881a28fa2a88
Syzuna <syzuna@t-teske.de>
2018-03-08 16:40:59 +0100

    changed to HttpClient fpr api
    changed to HttpClient fpr api and added a custom handler to intercept requests and log them

---
+++ TwitchLib.Api/Internal/TwitchLibCustomHttpMessageHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace TwitchLib.Api.Internal
+{
+    public class TwitchLibCustomHttpMessageHandler : DelegatingHandler
+    {
+        private readonly ILogger<TwitchAPI> _logger;
+
+        public TwitchLibCustomHttpMessageHandler(HttpMessageHandler innerHandler, ILogger<TwitchAPI> logger) : base(innerHandler)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Content != null)
+                _logger.LogInformation("Timestamp: {timestamp} Type: {type} Resource: {url} Content: {content}",
+                    DateTime.Now, request.RequestUri.ToString(), "Request", await request.Content.ReadAsStringAsync());
+            else
+                _logger.LogInformation("Timestamp: {timestamp} Requested Resource: {url}",
+                    DateTime.Now, request.RequestUri.ToString());
+
+            var stopwatch = Stopwatch.StartNew();
+            var response = await base.SendAsync(request, cancellationToken);
+            stopwatch.Stop();
+
+            if (response.IsSuccessStatusCode)
+            {
+                if (response.Content != null)
+                    _logger.LogInformation("Timestamp: {timestamp} Type: {type} Resource: {url} Statuscode: {statuscode} Elapsed: {elapsed} ms Content: {content}",
+                        DateTime.Now, "Response", response.RequestMessage.RequestUri, response.StatusCode, stopwatch.ElapsedMilliseconds, await response.Content.ReadAsStringAsync());
+                else
+                    _logger.LogInformation("Timestamp: {timestamp} Type: {type} Resource: {url} Statuscode: {statuscode} Elapsed: {elapsed} ms",
+                        DateTime.Now, "Response", response.RequestMessage.RequestUri, response.StatusCode, stopwatch.ElapsedMilliseconds);
+            }
+            else
+            {
+                if (response.Content != null)
+                    _logger.LogError("Timestamp: {timestamp} Type: {type} Resource: {url} Statuscode: {statuscode} Elapsed: {elapsed} ms Content: {content}",
+                        DateTime.Now, "Response", response.RequestMessage.RequestUri, response.StatusCode, stopwatch.ElapsedMilliseconds, await response.Content.ReadAsStringAsync());
+                else
+                    _logger.LogError("Timestamp: {timestamp} Type: {type} Resource: {url} Statuscode: {statuscode} Elapsed: {elapsed} ms",
+                        DateTime.Now, "Response", response.RequestMessage.RequestUri, response.StatusCode, stopwatch.ElapsedMilliseconds);
+            }
+
+            return response;
+        }
+    }
+}
