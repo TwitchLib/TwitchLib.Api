ed7c7529b1f8e041a68bbba291302f796d1a4716
LuckyNoS7evin <andy.ramit@gmail.com>
2018-04-08 00:53:41 +0100

    Removed unused Async

--- TwitchLib.Api/ApiSettings.cs
+++ TwitchLib.Api/ApiSettings.cs
@@ -45,17 +45,17 @@ namespace TwitchLib.Api
       
         public Task<KeyValuePair<bool,string>> CheckCredentialsAsync()
         {
-            if (!string.IsNullOrWhiteSpace(ClientId) && !ValidClientIdAsync(ClientId))
+            if (!string.IsNullOrWhiteSpace(ClientId) && !ValidClientId(ClientId))
                 return Task.FromResult(new KeyValuePair<bool, string>(false, "The passed Client Id was not valid. To get a valid Client Id, register an application here: https://www.twitch.tv/kraken/oauth2/clients/new"));
             
-            if (!string.IsNullOrWhiteSpace(AccessToken) && !ValidAccessTokenAsync(AccessToken))
+            if (!string.IsNullOrWhiteSpace(AccessToken) && !ValidAccessToken(AccessToken))
                 return Task.FromResult(new KeyValuePair<bool, string>(false, "The passed Access Token was not valid. To get an access token, go here:  https://twitchtokengenerator.com/"));
 
             return Task.FromResult(new KeyValuePair<bool, string>(true, ""));
         }
 
         #region ValidClientId
-        private bool ValidClientIdAsync(string clientId)
+        private bool ValidClientId(string clientId)
         {
             try
             {
@@ -70,7 +70,7 @@ namespace TwitchLib.Api
         #endregion
 
         #region ValidAccessToken
-        private bool ValidAccessTokenAsync(string accessToken)
+        private bool ValidAccessToken(string accessToken)
         {
             try
             {
