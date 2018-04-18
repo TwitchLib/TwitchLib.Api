a52d7409113dd823ff16ee54fa028231b25b9fe3
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-04-18 00:10:05 -0600

    Adds GetGameAnalytics and the Analytics seciton

---
+++ TwitchLib.Api/Models/Helix/Analytics/GetGameAnalyticsResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchLib.Api.Models.Helix.Analytics
+{
+    public class GetGameAnalyticsResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public GameAnalytics[] GameId { get; protected set; }
+    }
+}
