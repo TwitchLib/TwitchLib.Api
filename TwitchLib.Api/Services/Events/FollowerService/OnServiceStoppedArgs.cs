8433d1cb1826423445fa788ad92a47c9ef60407e
RRickkert <RRickkert@hotmail.com>
2018-06-27 00:12:47 +0200

    Rewrote the followerservice to allow joining multiple channels

--- TwitchLib.Api/Services/Events/FollowerService/OnServiceStoppedArgs.cs
+++ TwitchLib.Api/Services/Events/FollowerService/OnServiceStoppedArgs.cs
@@ -1,16 +1,13 @@
-﻿using System;
-
-namespace TwitchLib.Api.Services.Events.FollowerService
+﻿namespace TwitchLib.Api.Services.Events.FollowerService
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <inheritdoc />
     /// <summary>Class representing event args for OnServiceStopped event.</summary>
     public class OnServiceStoppedArgs : EventArgs
     {
-        /// <summary>Event property representing channel the service is currently monitoring.</summary>
-        public string ChannelData;
-        /// <summary>Event property representing number of recent followers a query to Twitch Api should return.</summary>
-        public int QueryCount;
-        /// <summary>Event property representing seconds between queries to Twitch Api.</summary>
-        public int CheckIntervalSeconds;
+        /// <summary>Event property representing channel ids the service is currently monitoring.</summary>
+        public ICollection<string> ChannelIds;
     }
 }
