188773ceedc83b2dc66058d7cefaddc2edc505c4
Seungkeun Lee <sngn.lee@samsung.com>
2018-08-27 16:37:44 +0900

    Fix TopClipsResponse Cursor issue

--- TwitchLib.Api.V5.Models/Clips/TopClipsResponse.cs
+++ TwitchLib.Api.V5.Models/Clips/TopClipsResponse.cs
@@ -1,10 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 namespace TwitchLib.Api.V5.Models.Clips
 {
     public class TopClipsResponse
     {
+        [JsonProperty(PropertyName = "_cursor")]
         public string Cursor { get; protected set; }
         public List<Clip> Clips { get; protected set; } = new List<Clip>();
 
