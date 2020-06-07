56de9d99c3dde03878ab34b479b27fbf5de2b840
Robin <robin.scheper@malengo.nl>
2020-06-07 20:35:04 +0200

    this should be prefixed with an underscore

--- TwitchLib.Api.Helix.Models/Games/Game.cs
+++ TwitchLib.Api.Helix.Models/Games/Game.cs
@@ -4,7 +4,7 @@ namespace TwitchLib.Api.Helix.Models.Games
 {
     public class Game
     {
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "_id")]
         public string Id { get; protected set; }
         [JsonProperty(PropertyName = "name")]
         public string Name { get; protected set; }
