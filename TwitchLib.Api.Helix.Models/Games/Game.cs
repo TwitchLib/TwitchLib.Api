b35db6a8a92e1e62067a2060e78307454f717b17
Robin <robin.scheper@malengo.nl>
2020-06-07 20:38:46 +0200

    this should be an int

--- TwitchLib.Api.Helix.Models/Games/Game.cs
+++ TwitchLib.Api.Helix.Models/Games/Game.cs
@@ -5,7 +5,7 @@ namespace TwitchLib.Api.Helix.Models.Games
     public class Game
     {
         [JsonProperty(PropertyName = "_id")]
-        public string Id { get; protected set; }
+        public int Id { get; protected set; }
         [JsonProperty(PropertyName = "name")]
         public string Name { get; protected set; }
         [JsonProperty(PropertyName = "box_art_url")]
