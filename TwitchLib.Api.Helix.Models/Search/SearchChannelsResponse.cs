a2f6f3affadefcfd948af5be65f9ab6c734582e9
kampar <kamil.paruszkiewicz@gmail.com>
2020-10-12 21:25:13 +0200

    Added Helix Search requests

---
+++ TwitchLib.Api.Helix.Models/Search/SearchChannelsResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using TwitchLib.Api.Helix.Models.Common;
+
+namespace TwitchLib.Api.Helix.Models.Search
+{
+    public class SearchChannelsResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public Channel[] Channels { get; protected set; }
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; protected set; }
+    }
+}
