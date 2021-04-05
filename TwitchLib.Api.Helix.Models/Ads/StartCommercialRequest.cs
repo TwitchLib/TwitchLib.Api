0febe5f4665c73c0f9da4e07eb7a415657f7ea98
pgrote <patrick.grote@dmxcontrol.de>
2021-04-05 02:08:11 +0200

    Fix Ads not part of Helix class

--- TwitchLib.Api.Helix.Models/Ads/StartCommercialRequest.cs
+++ TwitchLib.Api.Helix.Models/Ads/StartCommercialRequest.cs
@@ -8,8 +8,8 @@ namespace TwitchLib.Api.Helix.Models.Ads
     public class StartCommercialRequest
     {
         [JsonProperty(PropertyName = "broadcaster_id")]
-        public string BroadcasterId { get; protected set; }
+        public string BroadcasterId { get; set; }
         [JsonProperty(PropertyName = "length")]
-        public int Length { get; protected set; }
+        public int Length { get; set; }
     }
 }
