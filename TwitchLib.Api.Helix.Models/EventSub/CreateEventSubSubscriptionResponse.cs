98f3af3e9b757b845fa2b801aeb2feef5d43d082
Syzuna <syzuna@t-teske.de>
2021-04-16 20:43:14 +0200

    EventSub API support (#180)
    * finally added forgotten EventSub API support
    
    * added pagination support to GetEventSubSubscriptions request

---
+++ TwitchLib.Api.Helix.Models/EventSub/CreateEventSubSubscriptionResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitchLib.Api.Helix.Models.EventSub
+{
+    public class CreateEventSubSubscriptionResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public EventSubSubscription[] Subscriptions { get; protected set; }
+        [JsonProperty(PropertyName = "total")]
+        public int Total { get; protected set; }
+        [JsonProperty(PropertyName = "total_cost")]
+        public int TotalCost { get; protected set; }
+        [JsonProperty(PropertyName = "max_total_cost")]
+        public int MaxTotalCost { get; protected set; }
+    }
+}
---
+++ TwitchLib.Api.Helix.Models/EventSub/EventSubTransport.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitchLib.Api.Helix.Models.EventSub
+{
+    public class EventSubTransport
+    {
+        [JsonProperty(PropertyName = "method")]
+        public string Method { get; protected set; }
+        [JsonProperty(PropertyName = "callback")]
+        public string Callback { get; protected set; }
+    }
+}
