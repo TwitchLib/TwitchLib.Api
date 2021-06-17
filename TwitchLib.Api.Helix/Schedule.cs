2a7a5ea453ff87b00e7b8f1db3d27a5121ea56aa
Syzuna <syzuna@t-teske.de>
2021-06-17 02:04:59 +0200

    added Schedule Api section

---
+++ TwitchLib.Api.Helix/Schedule.cs
@@ -0,0 +1,11 @@
+﻿using TwitchLib.Api.Core;
+using TwitchLib.Api.Core.Interfaces;
+
+namespace TwitchLib.Api.Helix
+{
+    public class Schedule : ApiBase
+    {
+        public Schedule(IApiSettings settings, IRateLimiter rateLimiter, IHttpCallHandler http) : base(settings, rateLimiter, http)
+        { }
+    }
+}
