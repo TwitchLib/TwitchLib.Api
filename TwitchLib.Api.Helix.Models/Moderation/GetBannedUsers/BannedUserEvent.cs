66f9b5e40385227f3195863c8fbb49f981834e9b
Syzuna <syzuna@t-teske.de>
2021-07-23 22:15:21 +0200

    added newly added fields to GetBannedUsers and GetBannedEvents

--- TwitchLib.Api.Helix.Models/Moderation/GetBannedUsers/BannedUserEvent.cs
+++ TwitchLib.Api.Helix.Models/Moderation/GetBannedUsers/BannedUserEvent.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TwitchLib.Api.Helix.Models.Moderation.GetBannedUsers
 {
@@ -15,5 +13,13 @@ namespace TwitchLib.Api.Helix.Models.Moderation.GetBannedUsers
         public string UserName { get; protected set; }
         [JsonProperty(PropertyName = "expires_at")]
         public DateTime? ExpiresAt { get; protected set; }
+        [JsonProperty(PropertyName = "reason")]
+        public string Reason { get; protected set; }
+        [JsonProperty(PropertyName = "moderator_id")]
+        public string ModeratorId { get; protected set; }
+        [JsonProperty(PropertyName = "moderator_login")]
+        public string ModeratorLogin { get; protected set; }
+        [JsonProperty(PropertyName = "moderator_name")]
+        public string ModeratorName { get; protected set; }
     }
 }
