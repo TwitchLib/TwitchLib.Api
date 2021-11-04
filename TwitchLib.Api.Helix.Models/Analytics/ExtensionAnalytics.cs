6a67cde9e0980daa699ccb7b75c447c051b0c4a1
Mahsaap <vimhatt@hotmail.com>
2021-11-04 16:01:53 -0300

    Update ExtensionAnalytics (#224)
    * Update ExtensionAnalytics

--- TwitchLib.Api.Helix.Models/Analytics/ExtensionAnalytics.cs
+++ TwitchLib.Api.Helix.Models/Analytics/ExtensionAnalytics.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace TwitchLib.Api.Helix.Models.Analytics
 {
@@ -8,5 +8,9 @@ namespace TwitchLib.Api.Helix.Models.Analytics
         public string ExtensionId { get; protected set; }
         [JsonProperty(PropertyName = "URL")]
         public string Url { get; protected set; }
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; protected set; }
+        [JsonProperty(PropertyName = "date_range")]
+        public Common.DateRange DateRange { get; protected set; }
     }
 }
