9039959158b55cca9f75a2ed3bcdf2b68c34c2c5
Cole <swiftyspiffy@gmail.com>
2021-12-04 17:53:41 -0800

    remove GetStreamsMetadata per #238 (#241)

--- TwitchLib.Api.Helix/Streams.cs
+++ TwitchLib.Api.Helix/Streams.cs
@@ -12,7 +12,6 @@ using TwitchLib.Api.Helix.Models.Streams.GetStreamKey;
 using TwitchLib.Api.Helix.Models.Streams.GetStreamMarkers;
 using TwitchLib.Api.Helix.Models.Streams.GetStreams;
 using TwitchLib.Api.Helix.Models.Streams.GetStreamTags;
-using TwitchLib.Api.Helix.Models.StreamsMetadata;
 
 namespace TwitchLib.Api.Helix
 {
@@ -62,48 +61,6 @@ namespace TwitchLib.Api.Helix
             }
 
             return TwitchGetGenericAsync<GetStreamsResponse>($"/streams", ApiVersion.Helix, getParams, accessToken);
-        }
-
-        public Task<GetStreamsMetadataResponse> GetStreamsMetadataAsync(string after = null, List<string> communityIds = null, int first = 20, List<string> gameIds = null, List<string> languages = null, string type = "all", List<string> userIds = null, List<string> userLogins = null, string accessToken = null)
-        {
-            var getParams = new List<KeyValuePair<string, string>>
-                {
-                    new KeyValuePair<string, string>("first", first.ToString()),
-                    new KeyValuePair<string, string>("type", type)
-                };
-            if (after != null)
-                getParams.Add(new KeyValuePair<string, string>("after", after));
-            if (communityIds != null && communityIds.Count > 0)
-            {
-                foreach (var communityId in communityIds)
-                    getParams.Add(new KeyValuePair<string, string>("community_id", communityId));
-            }
-
-            if (gameIds != null && gameIds.Count > 0)
-            {
-                foreach (var gameId in gameIds)
-                    getParams.Add(new KeyValuePair<string, string>("game_id", gameId));
-            }
-
-            if (languages != null && languages.Count > 0)
-            {
-                foreach (var language in languages)
-                    getParams.Add(new KeyValuePair<string, string>("language", language));
-            }
-
-            if (userIds != null && userIds.Count > 0)
-            {
-                foreach (var userId in userIds)
-                    getParams.Add(new KeyValuePair<string, string>("user_id", userId));
-            }
-
-            if (userLogins != null && userLogins.Count > 0)
-            {
-                foreach (var userLogin in userLogins)
-                    getParams.Add(new KeyValuePair<string, string>("user_login", userLogin));
-            }
-
-            return TwitchGetGenericAsync<GetStreamsMetadataResponse>("/streams/metadata", ApiVersion.Helix, getParams, accessToken);
         }
 
         public Task<GetStreamTagsResponse> GetStreamTagsAsync(string broadcasterId, string accessToken = null)
