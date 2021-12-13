43a58d1c25ced9889ef828feea9511d3569d88aa
swiftyspiffy <swiftyspiffy@gmail.com>
2021-12-12 17:01:55 -0900

    add support for new Soundtrack apis

---
+++ TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylist/GetPlaylistResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TwitchLib.Api.Helix.Models.Soundtrack.GetPlaylist
+{
+    public class GetPlaylistResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public PlaylistTrack[] Data { get; protected set; }
+    }
+}
---
+++ TwitchLib.Api.Helix.Models/Soundtrack/Track.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TwitchLib.Api.Helix.Models.Soundtrack
+{
+    public class Track
+    {
+        [JsonProperty(PropertyName = "artists")]
+        public Artist[] Artists { get; protected set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; protected set; }
+        [JsonProperty(PropertyName = "duration")]
+        public int Duration { get; protected set; }
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; protected set; }
+        [JsonProperty(PropertyName = "album")]
+        public Album Album { get; protected set; }
+    }
+}
