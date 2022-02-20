2d8eb9620da940457956f0ba9350fbca1675dd7e
swiftyspiffy <swiftyspiffy@gmail.com>
2022-02-20 14:39:41 +0200

    add to_login

--- TwitchLib.Api.Helix.Models/Users/GetUserFollows/Follow.cs
+++ TwitchLib.Api.Helix.Models/Users/GetUserFollows/Follow.cs
@@ -13,6 +13,8 @@ namespace TwitchLib.Api.Helix.Models.Users.GetUserFollows
         public string FromUserName { get; protected set; }
         [JsonProperty(PropertyName = "to_id")]
         public string ToUserId { get; protected set; }
+        [JsonProperty(PropertyName = "to_login")]
+        public string ToLogin { get; protected set; }
         [JsonProperty(PropertyName = "to_name")]
         public string ToUserName { get; protected set; }
         [JsonProperty(PropertyName = "followed_at")]
