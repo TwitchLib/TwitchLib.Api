4776b54c124740882d5e54af0eb74ce54d7d256f
Mahsaap <mahsaaptwitch@gmail.com>
2022-06-30 23:08:48 -0300

    Update Soundtrack Calls / Models / XML Comments

--- TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylists/GetPlaylistsResponse.cs
+++ TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylists/GetPlaylistsResponse.cs
@@ -3,8 +3,14 @@ using Newtonsoft.Json;
 
 namespace TwitchLib.Api.Helix.Models.Soundtrack.GetPlaylists
 {
+    /// <summary>
+    /// Returns a list of Soundtrack playlists.
+    /// </summary>
     public class GetPlaylistsResponse
     {
+        /// <summary>
+        /// The list of Soundtrack playlists.
+        /// </summary>
         [JsonProperty(PropertyName = "data")]
         public PlaylistMetadata[] Data { get; protected set; }
     }
