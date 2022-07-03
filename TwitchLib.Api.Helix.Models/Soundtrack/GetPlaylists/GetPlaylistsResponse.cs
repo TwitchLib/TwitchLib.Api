676006734e708eb134e6648d19f8de2e81032412
Mahsaap <mahsaaptwitch@gmail.com>
2022-07-03 01:50:02 -0300

    Update Soundtrack Calls / Models / XML Comments (Redo)

--- TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylists/GetPlaylistsResponse.cs
+++ TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylists/GetPlaylistsResponse.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using Newtonsoft.Json;
+using TwitchLib.Api.Helix.Models.Common;
 
 namespace TwitchLib.Api.Helix.Models.Soundtrack.GetPlaylists
 {
+    /// <summary>
+    /// Returns a list of Soundtrack playlists.
+    /// </summary>
     public class GetPlaylistsResponse
     {
+        /// <summary>
+        /// The list of Soundtrack playlists.
+        /// </summary>
         [JsonProperty(PropertyName = "data")]
         public PlaylistMetadata[] Data { get; protected set; }
+
+        /// <summary>
+        /// Contains the information used to page through a list of playlists. The object is empty if there are no more playlists to page through.
+        /// </summary>
+        [JsonProperty(PropertyName = "pagination")]
+        public Pagination Pagination { get; protected set; }
     }
 }
--- TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylists/PlaylistMetadata.cs
+++ TwitchLib.Api.Helix.Models/Soundtrack/GetPlaylists/PlaylistMetadata.cs
@@ -3,14 +3,32 @@ using Newtonsoft.Json;
 
 namespace TwitchLib.Api.Helix.Models.Soundtrack.GetPlaylists
 {
+    /// <summary>
+    /// Soundtrack playlist.
+    /// </summary>
     public class PlaylistMetadata
     {
+        /// <summary>
+        /// The playlist’s title.
+        /// </summary>
         [JsonProperty(PropertyName = "title")]
         public string Title { get; protected set; }
+
+        /// <summary>
+        /// The playlist’s ASIN (Amazon Standard Identification Number).
+        /// </summary>
         [JsonProperty(PropertyName = "id")]
         public string Id { get; protected set; }
+
+        /// <summary>
+        /// A URL to the playlist’s image art. Is empty if the playlist doesn’t include art.
+        /// </summary>
         [JsonProperty(PropertyName = "image_url")]
         public string ImageUrl { get; protected set; }
+
+        /// <summary>
+        /// A short description about the music that the playlist includes.
+        /// </summary>
         [JsonProperty(PropertyName = "description")]
         public string Description { get; protected set; }
     }
