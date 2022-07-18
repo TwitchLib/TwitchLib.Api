5b78d35d3d6c41601bd469a0d3b42c46ea2e06bb
Mahsaap <mahsaaptwitch@gmail.com>
2022-07-17 23:31:54 -0300

    Fix naming in UserColors class

--- TwitchLib.Api.Helix.Models/Chat/UserColors.cs
+++ TwitchLib.Api.Helix.Models/Chat/UserColors.cs
@@ -11,18 +11,18 @@ namespace TwitchLib.Api.Helix.Models.Chat
         public string Value { get; private set; }
 
         public static UserColors Blue { get { return new UserColors("blue"); } }
-        public static UserColors Blue_Voilet { get { return new UserColors("blue_violet"); } }
-        public static UserColors Cadet_Blue { get { return new UserColors("cadet_blue"); } }
+        public static UserColors BlueVoilet { get { return new UserColors("blue_violet"); } }
+        public static UserColors CadetBlue { get { return new UserColors("cadet_blue"); } }
         public static UserColors Chocolate { get { return new UserColors("chocolate"); } }
         public static UserColors Coral { get { return new UserColors("coral"); } }
-        public static UserColors Dodger_Blue { get { return new UserColors("dodger_blue"); } }
+        public static UserColors DodgerBlue { get { return new UserColors("dodger_blue"); } }
         public static UserColors Firebrick { get { return new UserColors("firebrick"); } }
-        public static UserColors Golden_Rod { get { return new UserColors("golden_rod"); } }
-        public static UserColors Hot_Pink { get { return new UserColors("hot_pink"); } }
-        public static UserColors Orange_Red { get { return new UserColors("orange_red"); } }
+        public static UserColors GoldenRod { get { return new UserColors("golden_rod"); } }
+        public static UserColors HotPink { get { return new UserColors("hot_pink"); } }
+        public static UserColors OrangeRed { get { return new UserColors("orange_red"); } }
         public static UserColors Red { get { return new UserColors("red"); } }
-        public static UserColors Sea_Green { get { return new UserColors("sea_green"); } }
-        public static UserColors Spring_Green { get { return new UserColors("spring_green"); } }
-        public static UserColors Yellow_Green { get { return new UserColors("yellow_green"); } }
+        public static UserColors SeaGreen { get { return new UserColors("sea_green"); } }
+        public static UserColors SpringGreen { get { return new UserColors("spring_green"); } }
+        public static UserColors YellowGreen { get { return new UserColors("yellow_green"); } }
     }
 }
