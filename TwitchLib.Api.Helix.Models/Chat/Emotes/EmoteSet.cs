ed729d1e1273bd44b1c48429a33e273f28206476
Mahsaap <mahsaaptwitch@gmail.com>
2022-07-19 23:26:42 -0300

    Add XML Comments and missing properties in the emote models

--- TwitchLib.Api.Helix.Models/Chat/Emotes/EmoteSet.cs
+++ TwitchLib.Api.Helix.Models/Chat/Emotes/EmoteSet.cs
@@ -4,10 +4,19 @@ namespace TwitchLib.Api.Helix.Models.Chat.Emotes
 {
     public class EmoteSet : Emote
     {
+        /// <summary>
+        /// The type of emote. 
+        /// </summary>
         [JsonProperty("emote_type")]
         public string EmoteType { get; protected set; }
+        /// <summary>
+        /// An ID that identifies the emote set that the emote belongs to.
+        /// </summary>
         [JsonProperty("emote_set_id")]
         public string EmoteSetId { get; protected set; }
+        /// <summary>
+        /// The ID of the broadcaster who owns the emote.
+        /// </summary>
         [JsonProperty("owner_id")]
         public string OwnerId { get; protected set; }
     }
