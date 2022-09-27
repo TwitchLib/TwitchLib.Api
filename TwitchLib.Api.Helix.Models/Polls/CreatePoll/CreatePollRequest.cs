b39bc744697f0d5299743a08b7a3244e086a7c3c
Mahsaap <mahsaaptwitch@gmail.com>
2022-09-26 22:40:03 -0300

    Changelog 2022-09-01 - Removal warning added instead of removing - Not breaking change.

--- TwitchLib.Api.Helix.Models/Polls/CreatePoll/CreatePollRequest.cs
+++ TwitchLib.Api.Helix.Models/Polls/CreatePoll/CreatePollRequest.cs
@@ -10,8 +10,10 @@ namespace TwitchLib.Api.Helix.Models.Polls.CreatePoll
         public string Title { get; set; }
         [JsonProperty(PropertyName = "choices")]
         public Choice[] Choices { get; set; }
+        /// <summary> Removed - Ignored if included with call. Defaults to false</summary>
         [JsonProperty(PropertyName = "bits_voting_enabled")]
         public bool BitsVotingEnabled { get; set; }
+        /// <summary> Removed - Ignored if included with call. Defaults to 0</summary>
         [JsonProperty(PropertyName = "bits_per_vote")]
         public int BitsPerVote { get; set; }
         [JsonProperty(PropertyName = "channel_points_voting_enabled")]
