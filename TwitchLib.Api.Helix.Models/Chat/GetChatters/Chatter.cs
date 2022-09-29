879aaae5addcdb567a2b033014f52b1194e04c2b
Tobias "Syzuna" Teske <syzuna@syzuna-programs.de>
2022-09-29 23:02:55 +0200

    add missed total to GetChatters model and make setters protected

--- TwitchLib.Api.Helix.Models/Chat/GetChatters/Chatter.cs
+++ TwitchLib.Api.Helix.Models/Chat/GetChatters/Chatter.cs
@@ -8,6 +8,6 @@ namespace TwitchLib.Api.Helix.Models.Chat.GetChatters
         /// The login name of a user that’s connected to the broadcaster’s chat room.
         /// </summary>
         [JsonProperty("user_login")]
-        public string UserLogin { get; set; }
+        public string UserLogin { get; protected set; }
     }
 }
