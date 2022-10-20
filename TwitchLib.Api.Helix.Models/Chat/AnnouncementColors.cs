9976593a0e01c80a7ca055295e2ee74b971c83c0
iProdigy <iProdigy@users.noreply.github.com>
2022-10-19 22:26:27 -0700

    fix: correct primary announcement color value

--- TwitchLib.Api.Helix.Models/Chat/AnnouncementColors.cs
+++ TwitchLib.Api.Helix.Models/Chat/AnnouncementColors.cs
@@ -10,6 +10,6 @@
         public static AnnouncementColors Green { get { return new AnnouncementColors("green"); } }
         public static AnnouncementColors Orange { get { return new AnnouncementColors("orange"); } }
         public static AnnouncementColors Purple { get { return new AnnouncementColors("purple"); } }
-        public static AnnouncementColors Primary { get { return new AnnouncementColors("primary "); } }
+        public static AnnouncementColors Primary { get { return new AnnouncementColors("primary"); } }
     }
 }
