f564f9d2f03c5e85f9f67980dad7b644a51d9ef2
Tobias "Syzuna" Teske <syzuna@syzuna-programs.de>
2022-10-22 22:34:58 +0200

    xml docs part 3

--- TwitchLib.Api.Helix/Games.cs
+++ TwitchLib.Api.Helix/Games.cs
@@ -9,64 +9,87 @@ using TwitchLib.Api.Helix.Models.Games;
 
 namespace TwitchLib.Api.Helix
 {
+    /// <summary>
+    /// Games related APIs
+    /// </summary>
     public class Games : ApiBase
+    {
+        public Games(IApiSettings settings, IRateLimiter rateLimiter, IHttpCallHandler http) : base(settings, rateLimiter, http)
         {
-            public Games(IApiSettings settings, IRateLimiter rateLimiter, IHttpCallHandler http) : base(settings, rateLimiter, http)
+        }
+
+        #region GetGames
+        /// <summary>
+        /// Gets game information by game ID or name.
+        /// <para>For a query to be valid, name and/or id must be specified.</para>
+        /// </summary>
+        /// <param name="gameIds">List of Game IDs. At most 100 id values can be specified.</param>
+        /// <param name="gameNames">
+        /// List of Game names.
+        /// <para>The name must be an exact match. For example, “Pokemon” will not return a list of Pokemon games; instead, query any specific Pokemon games in which you are interested.</para>
+        /// <para>At most 100 name values can be specified.</para>
+        /// </param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        /// <returns cref="GetGamesResponse"></returns>
+        /// <exception cref="BadParameterException"></exception>
+        public Task<GetGamesResponse> GetGamesAsync(List<string> gameIds = null, List<string> gameNames = null, string accessToken = null)
+        {
+            if (gameIds == null && gameNames == null || gameIds != null && gameIds.Count == 0 && gameNames == null || gameNames != null && gameNames.Count == 0 && gameIds == null)
+                throw new BadParameterException("Either gameIds or gameNames must have at least one value");
+
+            if (gameIds != null && gameIds.Count > 100)
+                throw new BadParameterException("gameIds list cannot exceed 100 items");
+
+            if (gameNames != null && gameNames.Count > 100)
+                throw new BadParameterException("gameNames list cannot exceed 100 items");
+
+            var getParams = new List<KeyValuePair<string, string>>();
+
+            if (gameIds != null && gameIds.Count > 0)
             {
+                getParams.AddRange(gameIds.Select(gameId => new KeyValuePair<string, string>("id", gameId)));
             }
 
-            #region GetGames
-
-            public Task<GetGamesResponse> GetGamesAsync(List<string> gameIds = null, List<string> gameNames = null, string accessToken = null)
+            if (gameNames != null && gameNames.Count > 0)
             {
-                if (gameIds == null && gameNames == null || gameIds != null && gameIds.Count == 0 && gameNames == null || gameNames != null && gameNames.Count == 0 && gameIds == null)
-                    throw new BadParameterException("Either gameIds or gameNames must have at least one value");
-
-                if (gameIds != null && gameIds.Count > 100)
-                    throw new BadParameterException("gameIds list cannot exceed 100 items");
-
-                if (gameNames != null && gameNames.Count > 100)
-                    throw new BadParameterException("gameNames list cannot exceed 100 items");
-
-                var getParams = new List<KeyValuePair<string, string>>();
-
-                if (gameIds != null && gameIds.Count > 0)
-                {
-                    getParams.AddRange(gameIds.Select(gameId => new KeyValuePair<string, string>("id", gameId)));
-                }
-
-                if (gameNames != null && gameNames.Count > 0)
-                {
-                    getParams.AddRange(gameNames.Select(gameName => new KeyValuePair<string, string>("name", gameName)));
-                }
-
-                return TwitchGetGenericAsync<GetGamesResponse>("/games", ApiVersion.Helix, getParams, accessToken);
+                getParams.AddRange(gameNames.Select(gameName => new KeyValuePair<string, string>("name", gameName)));
             }
 
-            #endregion
+            return TwitchGetGenericAsync<GetGamesResponse>("/games", ApiVersion.Helix, getParams, accessToken);
+        }
 
-            #region GetTopGames
+        #endregion
 
-            public Task<GetTopGamesResponse> GetTopGamesAsync(string before = null, string after = null, int first = 20, string accessToken = null)
-            {
-                if (first < 0 || first > 100)
-                    throw new BadParameterException("'first' parameter must be between 1 (inclusive) and 100 (inclusive).");
+        #region GetTopGames
+        /// <summary>
+        /// Gets games sorted by number of current viewers on Twitch, most popular first.
+        /// </summary>
+        /// <param name="before">Cursor for backward pagination: tells the server where to start fetching the next set of results, in a multi-page response. </param>
+        /// <param name="after">Cursor for forward pagination: tells the server where to start fetching the next set of results, in a multi-page response. </param>
+        /// <param name="first">Maximum number of objects to return. Maximum: 100. Default: 20.</param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        /// <returns cref="GetTopGamesResponse"></returns>
+        /// <exception cref="BadParameterException"></exception>
+        public Task<GetTopGamesResponse> GetTopGamesAsync(string before = null, string after = null, int first = 20, string accessToken = null)
+        {
+            if (first < 0 || first > 100)
+                throw new BadParameterException("'first' parameter must be between 1 (inclusive) and 100 (inclusive).");
 
-                var getParams = new List<KeyValuePair<string, string>>
+            var getParams = new List<KeyValuePair<string, string>>
                 {
                         new KeyValuePair<string, string>("first", first.ToString())
                 };
 
-                if (!string.IsNullOrWhiteSpace(before))
-                    getParams.Add(new KeyValuePair<string, string>("before", before));
+            if (!string.IsNullOrWhiteSpace(before))
+                getParams.Add(new KeyValuePair<string, string>("before", before));
 
-                if (!string.IsNullOrWhiteSpace(after))
-                    getParams.Add(new KeyValuePair<string, string>("after", after));
+            if (!string.IsNullOrWhiteSpace(after))
+                getParams.Add(new KeyValuePair<string, string>("after", after));
 
-                return TwitchGetGenericAsync<GetTopGamesResponse>("/games/top", ApiVersion.Helix, getParams, accessToken);
-            }
+            return TwitchGetGenericAsync<GetTopGamesResponse>("/games/top", ApiVersion.Helix, getParams, accessToken);
+        }
 
-            #endregion
-        
+        #endregion
+
     }
 }
--- TwitchLib.Api.Helix/Polls.cs
+++ TwitchLib.Api.Helix/Polls.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using TwitchLib.Api.Core;
 using TwitchLib.Api.Core.Enums;
 using TwitchLib.Api.Core.Interfaces;
@@ -18,6 +18,20 @@ namespace TwitchLib.Api.Helix
         {
         }
 
+        /// <summary>
+        /// Get information about all polls or specific polls for a Twitch channel. Poll information is available for 90 days.
+        /// <para>Required scope: channel:read:polls</para>
+        /// </summary>
+        /// <param name="broadcasterId">The broadcaster running polls. Provided broadcaster_id must match the user_id in the user OAuth token.</param>
+        /// <param name="ids">
+        /// IDs polls. Filters results to one or more specific polls.
+        /// <para>Not providing one or more IDs will return the full list of polls for the authenticated channel.</para>
+        /// <para>Maximum: 100</para>
+        /// </param>
+        /// <param name="after">Cursor for forward pagination: tells the server where to start fetching the next set of results in a multi-page response.</param>
+        /// <param name="first">Maximum number of objects to return. Maximum: 20. Default: 20.</param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        /// <returns></returns>
         public Task<GetPollsResponse> GetPollsAsync(string broadcasterId, List<string> ids = null, string after = null, int first = 20, string accessToken = null)
         {
             var getParams = new List<KeyValuePair<string, string>>
@@ -37,11 +51,31 @@ namespace TwitchLib.Api.Helix
             return TwitchGetGenericAsync<GetPollsResponse>("/polls", ApiVersion.Helix, getParams, accessToken);
         }
 
+        /// <summary>
+        /// Create a poll for a specific Twitch channel.
+        /// <para>Required scope: channel:manage:polls</para>
+        /// </summary>
+        /// <param name="request" cref="CreatePollRequest"></param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        /// <returns cref="CreatePollResponse"></returns>
         public Task<CreatePollResponse> CreatePollAsync(CreatePollRequest request, string accessToken = null)
         {
             return TwitchPostGenericAsync<CreatePollResponse>("/polls", ApiVersion.Helix, JsonConvert.SerializeObject(request), accessToken: accessToken);
         }
 
+        /// <summary>
+        /// End a poll that is currently active.
+        /// <para>Required scope: channel:manage:polls</para>
+        /// </summary>
+        /// <param name="broadcasterId">The broadcaster running polls. Provided broadcaster_id must match the user_id in the user OAuth token.</param>
+        /// <param name="id">ID of the poll to end.</param>
+        /// <param name="status" cref="PollStatusEnum">
+        /// The poll status to be set. Valid values:
+        /// <para>TERMINATED: End the poll manually, but allow it to be viewed publicly.</para>
+        /// <para>ARCHIVED: End the poll manually and do not allow it to be viewed publicly.</para>
+        /// </param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        /// <returns></returns>
         public Task<EndPollResponse> EndPollAsync(string broadcasterId, string id, PollStatusEnum status, string accessToken = null)
         {
             var json = new JObject
