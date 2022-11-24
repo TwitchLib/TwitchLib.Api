84c80e8bc715e418a1efa0b674e4dcc9b231b153
Vincent <vin@centheins.de>
2022-11-24 21:15:24 +0100

    Add type parameter to GetStreamsAsync (#323)

---
+++ TwitchLib.Api.Helix.Models/Streams/GetStreams/GetStreamsType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchLib.Api.Helix.Models.Streams.GetStreams
+{
+    /// <summary>
+    /// Stream type to select when using GetStreamsAsync
+    /// </summary>
+    public static class GetStreamsType
+    {
+        /// <summary>
+        /// Filters for all stream types.
+        /// </summary>
+        public const string All = "all";
+
+        /// <summary>
+        /// Filters for actual live streams.
+        /// </summary>
+        public const string Live = "live";
+    }
+}
