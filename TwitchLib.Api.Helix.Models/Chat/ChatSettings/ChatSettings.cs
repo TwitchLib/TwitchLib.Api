4a58c3083fd10ff1ac2cfa956a9dc7a69bdc53b8
Proddy <josh@zhbot.org>
2023-03-29 13:12:55 +0100

    Update ChatSettings.cs
    Primitive type `bool` defaults to `false` when not nullable. Making them nullable to prevent accidentally changing unwanted settings.

--- TwitchLib.Api.Helix.Models/Chat/ChatSettings/ChatSettings.cs
+++ TwitchLib.Api.Helix.Models/Chat/ChatSettings/ChatSettings.cs
@@ -8,21 +8,21 @@ namespace TwitchLib.Api.Helix.Models.Chat.ChatSettings
     public class ChatSettings
     {
         [JsonProperty(PropertyName = "slow_mode")]
-        public bool SlowMode;
+        public bool? SlowMode;
         [JsonProperty(PropertyName = "slow_mode_wait_time")]
         public int? SlowModeWaitTime;
         [JsonProperty(PropertyName = "follower_mode")]
-        public bool FollowerMode;
+        public bool? FollowerMode;
         [JsonProperty(PropertyName = "follower_mode_duration")]
         public int? FollowerModeDuration;
         [JsonProperty(PropertyName = "subscriber_mode")]
-        public bool SubscriberMode;
+        public bool? SubscriberMode;
         [JsonProperty(PropertyName = "emote_mode")]
-        public bool EmoteMode;
+        public bool? EmoteMode;
         [JsonProperty(PropertyName = "unique_chat_mode")]
-        public bool UniqueChatMode;
+        public bool? UniqueChatMode;
         [JsonProperty(PropertyName = "non_moderator_chat_delay")]
-        public bool NonModeratorChatDelay;
+        public bool? NonModeratorChatDelay;
         [JsonProperty(PropertyName = "non_moderator_chat_delay_duration")]
         public int? NonModeratorChatDelayDuration;
     }
