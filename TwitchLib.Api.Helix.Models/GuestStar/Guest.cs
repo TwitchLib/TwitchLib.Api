ae48cdeaa0d86dd6659920ba6ba5d8a76bd65874
Mahsaap <mahsaaptwitch@gmail.com>
2023-05-27 01:27:32 -0300

    Add Get Guest Star Session

---
+++ TwitchLib.Api.Helix.Models/GuestStar/Guest.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace TwitchLib.Api.Helix.Models.GuestStar
+{
+    public class Guest
+    {
+        /// <summary>
+        /// ID representing this guest’s slot assignment.
+        /// Host:0 Guests:1,2,3,etc ScreenShare:SCREENSHARE 
+        /// </summary>
+        [JsonProperty(PropertyName = "slot_id")]
+        public string SlotId { get; protected set; }
+        /// <summary>
+        /// Flag determining whether or not the guest is visible in the browser source in the host’s streaming software.
+        /// </summary>
+        [JsonProperty(PropertyName = "is_live")]
+        public bool IsLive { get; protected set; }
+        /// <summary>
+        /// User ID of the guest assigned to this slot.
+        /// </summary>
+        [JsonProperty(PropertyName = "user_id")]
+        public string UserId { get; protected set; }
+        /// <summary>
+        /// Display name of the guest assigned to this slot.
+        /// </summary>
+        [JsonProperty(PropertyName = "user_display_name")]
+        public string UserDisplayName { get; set; }
+        /// <summary>
+        /// Login of the guest assigned to this slot.
+        /// </summary>
+        [JsonProperty(PropertyName = "user_login")]
+        public string UserLogin { get; protected set; }
+        /// <summary>
+        /// Value from 0 to 100 representing the host’s volume setting for this guest.
+        /// </summary>
+        [JsonProperty(PropertyName = "volume")]
+        public int Volume { get; protected set; }
+        /// <summary>
+        /// Timestamp when this guest was assigned a slot in the session.
+        /// </summary>
+        [JsonProperty(PropertyName = "assigned_at")]
+        public DateTime AssignedAt { get; protected set; }
+        /// <summary>
+        /// Information about the guest’s audio settings
+        /// </summary>
+        [JsonProperty(PropertyName = "audio_settings")]
+        public MediaSettings AudioSettings { get; protected set; }
+        /// <summary>
+        /// Information about the guest’s audio settings
+        /// </summary>
+        [JsonProperty(PropertyName = "video_settings")]
+        public MediaSettings VideoSettings { get; protected set; }
+    }
+}
--- TwitchLib.Api.Helix/GuestStar.cs
+++ TwitchLib.Api.Helix/GuestStar.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TwitchLib.Api.Core;
 using TwitchLib.Api.Core.Enums;
 using TwitchLib.Api.Core.Exceptions;
 using TwitchLib.Api.Core.Interfaces;
-using TwitchLib.Api.Helix.Models.GuestStar;
+using TwitchLib.Api.Helix.Models.GuestStar.GetChannelGuestStarSettings;
+using TwitchLib.Api.Helix.Models.GuestStar.UpdateChannelGuestStarSettings;
 
 namespace TwitchLib.Api.Helix
 {
@@ -17,7 +19,7 @@ namespace TwitchLib.Api.Helix
         {
         }
 
-        #region GetCreatorGoals
+        #region GetChannelGuestStarSettings
         /// <summary>
         /// [BETA] Gets the channel settings for configuration of the Guest Star feature for a particular host.
         /// <para>Requires a user OAuth access token with scope set to channel:read:guest_star or channel:manage:guest_star. </para>
@@ -38,6 +40,58 @@ namespace TwitchLib.Api.Helix
             };
 
             return TwitchGetGenericAsync<GetChannelGuestStarSettingsResponse>("/guest_star/channel_settings", ApiVersion.Helix, getParams, accessToken);
+        }
+        #endregion
+
+        #region UpdateChannelGuestStarSettings
+
+        /// <summary>
+        /// [BETA] Updates the channel settings configuration of the Guest Star feature for a particular host.
+        /// <para>The Custom Reward specified by id must have been created by the ClientId attached to the user OAuth token.</para>
+        /// <para>Required scope: channel:manage:guest_star</para>
+        /// </summary>
+        /// <param name="broadcasterId">
+        /// The ID of the broadcaster you want to update Guest Star settings for.
+        /// </param>
+        /// <param name="request" cref="UpdateChannelGuestStarSettingsRequest"></param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        public Task UpdateCustomRewardAsync(string broadcasterId, UpdateChannelGuestStarSettingsRequest request, string accessToken = null)
+        {
+            var getParams = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("broadcaster_id", broadcasterId),
+            };
+
+            return TwitchPatchAsync("/guest_star/channel_settings", ApiVersion.Helix, JsonConvert.SerializeObject(request), getParams, accessToken);
+        }
+
+        #endregion
+
+        #region GetGuestStarSession
+        /// <summary>
+        /// [BETA] Gets information about an ongoing Guest Star session for a particular channel.
+        /// <para>Requires OAuth Scope: channel:read:guest_star, channel:manage:guest_star, moderator:read:guest_star or moderator:manage:guest_star </para>
+        /// <para>Guests must be either invited or assigned a slot within the session.</para>
+        /// </summary>
+        /// <param name="broadcasterId">ID for the user hosting the Guest Star session.</param>
+        /// <param name="moderatorId">The ID of the broadcaster or a user that has permission to moderate the broadcaster’s chat room. This ID must match the user ID in the user access token.</param>
+        /// <param name="accessToken">optional access token to override the use of the stored one in the TwitchAPI instance</param>
+        /// <returns cref="GetChannelGuestStarSettingsResponse"></returns>
+        /// <exception cref="BadParameterException"></exception>
+        public Task<GetGuestStarSessionResponse> GetGuestSessionAsync(string broadcasterId, string moderatorId, string accessToken = null)
+        {
+            if (string.IsNullOrEmpty(broadcasterId))
+                throw new BadParameterException("broadcasterId cannot be null or empty");
+            if (string.IsNullOrEmpty(moderatorId))
+                throw new BadParameterException("moderatorId cannot be null or empty");
+
+            var getParams = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("broadcaster_id", broadcasterId),
+                new KeyValuePair<string, string>("moderator_id", moderatorId)
+            };
+
+            return TwitchGetGenericAsync<GetGuestStarSessionResponse>("/guest_star/session", ApiVersion.Helix, getParams, accessToken);
         }
         #endregion
     }
