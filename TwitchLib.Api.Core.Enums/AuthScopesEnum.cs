7131b94d5cff113b6d747de9184ba4fd5d52b032
Mahsaap <mahsaaptwitch@gmail.com>
2023-06-10 00:09:31 -0300

    Fix again...

--- TwitchLib.Api.Core.Enums/AuthScopesEnum.cs
+++ TwitchLib.Api.Core.Enums/AuthScopesEnum.cs
@@ -3,32 +3,33 @@
     public enum AuthScopes
     {
         Any,
-
         Channel_Check_Subscription,
         Channel_Commercial,
         Channel_Editor,
         Channel_Feed_Edit,
         Channel_Feed_Read,
-        Channel_Moderate,
         Channel_Read,
         Channel_Stream,
         Channel_Subscriptions,
-
         Chat_Read,
         Chat_Edit,
-
+        Channel_Moderate,
         Collections_Edit,
-
         Communities_Edit,
         Communities_Moderate,
-
+        User_Blocks_Edit,
+        User_Blocks_Read,
+        User_Follows_Edit,
+        User_Read,
+        User_Subscriptions,
+        Viewing_Activity_Read,
+        OpenId,
         Helix_Analytics_Read_Extensions,
         Helix_Analytics_Read_Games,
         Helix_Bits_Read,
         Helix_Channel_Edit_Commercial,
         Helix_Channel_Manage_Broadcast,
         Helix_Channel_Manage_Extensions,
-        Helix_Channel_Manage_Guest_Star,
         Helix_Channel_Manage_Moderators,
         Helix_Channel_Manage_Polls,
         Helix_Channel_Manage_Predictions,
@@ -38,7 +39,6 @@
         Helix_Channel_Manage_VIPs,
         Helix_Channel_Read_Charity,
         Helix_Channel_Read_Editors,
-        Helix_Channel_Read_Guest_Star,
         Helix_Channel_Read_Goals,
         Helix_Channel_Read_Hype_Train,
         Helix_Channel_Read_Polls,
@@ -49,20 +49,17 @@
         Helix_Channel_Read_VIPs,
         Helix_Clips_Edit,
         Helix_Moderation_Read,
+        Helix_Moderator_Manage_Banned_Users,
+        Helix_Moderator_Manage_Blocked_Terms,
         Helix_Moderator_Manage_Announcements,
         Helix_Moderator_Manage_Automod,
         Helix_Moderator_Manage_Automod_Settings,
-        Helix_Moderator_Manage_Banned_Users,
-        Helix_Moderator_Manage_Blocked_Terms,
         Helix_moderator_Manage_Chat_Messages,
         Helix_Moderator_Manage_Chat_Settings,
-        Helix_Moderator_Manage_Guest_Star,
         Helix_Moderator_Read_Blocked_Terms,
         Helix_Moderator_Read_Automod_Settings,
         Helix_Moderator_Read_Chat_Settings,
         Helix_Moderator_Read_Chatters,
-        Helix_Moderator_Read_Followers,
-        Helix_Moderator_Read_Guest_Star,
         Helix_User_Edit,
         Helix_User_Edit_Broadcast,
         Helix_User_Edit_Follows,
@@ -74,17 +71,7 @@
         Helix_User_Read_Email,
         Helix_User_Read_Follows,
         Helix_User_Read_Subscriptions,
-        
-
-        User_Blocks_Edit,
-        User_Blocks_Read,
-        User_Follows_Edit,
-        User_Read,
-        User_Subscriptions,
-
-        Viewing_Activity_Read,
-
-        OpenId,
+        Helix_Moderator_Read_Followers,
         None
     }
 }
