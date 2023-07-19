6800786dbb320d953f2842bc57dfe8895f4c5974
koishibuh <elysiagriffin@gmail.com>
2023-07-19 15:40:16 +0100

    - Rearranged CreatedClip model properties to match Twitch API docs for easier comparison

--- TwitchLib.Api.Helix.Models/Clips/CreateClip/CreatedClip.cs
+++ TwitchLib.Api.Helix.Models/Clips/CreateClip/CreatedClip.cs
@@ -8,16 +8,16 @@ namespace TwitchLib.Api.Helix.Models.Clips.CreateClip
    public class CreatedClip
    {
       /// <summary>
-      /// An ID that uniquely identifies the clip.
-      /// </summary>
-      [JsonProperty(PropertyName = "id")]
-      public string Id { get; protected set; }
-
-      /// <summary>
       /// <para>A URL that you can use to edit the clip’s title, identify the part of the clip to publish, and publish the clip.</para>
       /// <para> The URL is valid for up to 24 hours or until the clip is published, whichever comes first.</para>
       /// </summary>
       [JsonProperty(PropertyName = "edit_url")]
       public string EditUrl { get; protected set; }
+
+      /// <summary>
+      /// An ID that uniquely identifies the clip.
+      /// </summary>
+      [JsonProperty(PropertyName = "id")]
+      public string Id { get; protected set; }
    }
 }
