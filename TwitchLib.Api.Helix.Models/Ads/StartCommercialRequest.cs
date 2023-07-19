f8abb3884f608fbdf54589132865ac8d812967d7
koishibuh <elysiagriffin@gmail.com>
2023-07-19 13:51:16 +0100

    - Added summaries to Ad Models

--- TwitchLib.Api.Helix.Models/Ads/StartCommercialRequest.cs
+++ TwitchLib.Api.Helix.Models/Ads/StartCommercialRequest.cs
@@ -2,11 +2,26 @@
 
 namespace TwitchLib.Api.Helix.Models.Ads
 {
-    public class StartCommercialRequest
-    {
-        [JsonProperty(PropertyName = "broadcaster_id")]
-        public string BroadcasterId { get; set; }
-        [JsonProperty(PropertyName = "length")]
-        public int Length { get; set; }
-    }
+   /// <summary>
+   /// <para>Starts a commercial on the specified channel. </para>
+   /// <para>Only partners and affiliates may run commercials and they must be streaming live at the time.</para>
+   /// <para>Only the broadcaster may start a commercial; the broadcaster’s editors and moderators may not start commercials on behalf of the broadcaster.</para>
+   /// </summary>
+   public class StartCommercialRequest
+   {
+      /// <summary>
+      /// The ID of the partner or affiliate broadcaster that wants to run the commercial. 
+      /// This ID must match the user ID found in the OAuth token.
+      /// </summary>
+      [JsonProperty(PropertyName = "broadcaster_id")]
+      public string BroadcasterId { get; set; }
+
+      /// <summary>
+      /// The length of the commercial to run, in seconds.
+      /// Twitch tries to serve a commercial that’s the requested length, but it may be shorter or longer. 
+      /// The maximum length you should request is 180 seconds.
+      /// </summary>
+      [JsonProperty(PropertyName = "length")]
+      public int Length { get; set; }
+   }
 }
