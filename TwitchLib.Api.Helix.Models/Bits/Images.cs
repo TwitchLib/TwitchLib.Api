09a6630ee086775ac8016075163349eb23b7a2c6
koishibuh <elysiagriffin@gmail.com>
2023-07-19 15:22:52 +0100

    - Added summaries to GetCheermote models

--- TwitchLib.Api.Helix.Models/Bits/Images.cs
+++ TwitchLib.Api.Helix.Models/Bits/Images.cs
@@ -2,11 +2,27 @@ using Newtonsoft.Json;
 
 namespace TwitchLib.Api.Helix.Models.Bits
 {
-    public class Images
-    {
-        [JsonProperty(PropertyName = "dark")]
-        public ImageList Dark { get; protected set; }
-        [JsonProperty(PropertyName = "light")]
-        public ImageList Light { get; protected set; }
-    }
+   /// <summary>
+   /// <para>The dark and light themes of a Cheermote.</para>
+   /// <para>Each theme has static and animated formats of the Cheermote. Each format has a dictionary
+   /// containing multiple image sizes and associated image URLs.</para>
+   ///</summary>
+   public class Images
+   {
+      /// <summary>
+      /// <para>Dark theme key for a Cheermote.</para>
+      /// <para>Contains the static and animated formats for the Cheermote. 
+      /// Each format has a dictionary containing multiple image sizes and associated image URLs.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "dark")]
+      public ImageList Dark { get; protected set; }
+
+      /// <summary>
+      /// <para>Light theme key for a Cheermote.</para>
+      /// <para>Contains the static and animated formats for the Cheermote. 
+      /// Each format has a dictionary containing multiple image sizes and associated image URLs.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "light")]
+      public ImageList Light { get; protected set; }
+   }
 }
