7cccadbf5f5fd7d9103d90d91688d959da374822
koishibuh <elysiagriffin@gmail.com>
2023-07-20 16:07:38 +0100

    - Updated the GetFollowedChannel models

--- TwitchLib.Api.Helix.Models/Channels/GetFollowedChannels/GetFollowedChannelsResponse.cs
+++ TwitchLib.Api.Helix.Models/Channels/GetFollowedChannels/GetFollowedChannelsResponse.cs
@@ -3,26 +3,32 @@ using TwitchLib.Api.Helix.Models.Common;
 
 namespace TwitchLib.Api.Helix.Models.Channels.GetFollowedChannels
 {
-    public class GetFollowedChannelsResponse
-    {
-        /// <summary>
-        /// The list of broadcasters that the user follows.
-        /// <para>The list is in descending order by followed_at (with the most recently followed broadcaster first).</para>
-        /// <para>The list is empty if the user doesn’t follow anyone.</para>
-        /// </summary>
-        [JsonProperty(PropertyName = "data")]
-        public FollowedChannel[] Data { get; protected set; }
-        
-        /// <summary>
-        /// Contains the information used to page through the list of results. The object is empty if there are no more pages left to page through.
-        /// </summary>
-        [JsonProperty(PropertyName = "pagination")]
-        public Pagination Pagination { get; protected set; }
-        
-        /// <summary>
-        /// The total number of broadcasters that the user follows. As someone pages through the list, the number may change as the user follows or unfollows broadcasters.
-        /// </summary>
-        [JsonProperty(PropertyName = "total")]
-        public int Total { get; protected set; }
-    }
+   /// <summary>
+   /// <para>The response for GetFollowedChannels that returns a list of broadcasters that the specified user follows.</para>
+   /// <para>This can also return whether a user follows a specific broadcaster.</para>
+   /// </summary>
+   public class GetFollowedChannelsResponse
+   {
+      /// <summary>
+      /// <para>The list of broadcasters that the user follows.</para>
+      /// <para>The list is in descending order by followed_at, with the most recently followed broadcaster first.</para>
+      /// <para>The list is empty if the user doesn’t follow anyone.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "data")]
+      public FollowedChannel[] Data { get; protected set; }
+
+      /// <summary>
+      /// <para>Contains the information used to page through the list of results.</para>
+      /// <para>The object is empty if there are no more pages left to page through.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "pagination")]
+      public Pagination Pagination { get; protected set; }
+
+      /// <summary>
+      /// <para>The total number of broadcasters that the user follows.</para>
+      /// <para>As someone pages through the list, the number may change as the user follows or unfollows broadcasters.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "total")]
+      public int Total { get; protected set; }
+   }
 }
