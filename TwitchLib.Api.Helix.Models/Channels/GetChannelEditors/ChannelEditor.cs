8a247bd81fd1143bf245b099edc26c1b30579606
koishibuh <elysiagriffin@gmail.com>
2023-07-20 14:10:18 +0100

    - Added summaries to GetChannelEditors Models

--- TwitchLib.Api.Helix.Models/Channels/GetChannelEditors/ChannelEditor.cs
+++ TwitchLib.Api.Helix.Models/Channels/GetChannelEditors/ChannelEditor.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace TwitchLib.Api.Helix.Models.Channels.GetChannelEditors
 {
-    public class ChannelEditor
-    {
-        [JsonProperty(PropertyName = "user_id")]
-        public string UserId { get; protected set; }
-        [JsonProperty(PropertyName = "user_name")]
-        public string UserName { get; protected set; }
-        [JsonProperty(PropertyName = "created_at")]
-        public DateTime CreatedAt { get; protected set; }
-    }
+   /// <summary>
+   /// <para>A user that is a channel editor for the broadcaster.</para>
+   /// </summary>
+   public class ChannelEditor
+   {
+      /// <summary>
+      /// <para>An ID that uniquely identifies a user with editor permissions.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_id")]
+      public string UserId { get; protected set; }
+
+      /// <summary>
+      /// <para>The user’s display name.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_name")]
+      public string UserName { get; protected set; }
+
+      /// <summary>
+      /// <para>The date and time when the user became one of the broadcaster’s editors.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "created_at")]
+      public DateTime CreatedAt { get; protected set; }
+   }
 }
--- TwitchLib.Api.Helix.Models/Channels/GetChannelEditors/GetChannelEditorsResponse.cs
+++ TwitchLib.Api.Helix.Models/Channels/GetChannelEditors/GetChannelEditorsResponse.cs
@@ -2,9 +2,16 @@
 
 namespace TwitchLib.Api.Helix.Models.Channels.GetChannelEditors
 {
-    public class GetChannelEditorsResponse
-    {
-        [JsonProperty(PropertyName = "data")]
-        public ChannelEditor[] Data { get; protected set; }
-    }
+   /// <summary>
+   /// <para>Response for GetChannelEditors that returns a list of the broadcaster's channels editors.</para>
+   /// </summary>
+   public class GetChannelEditorsResponse
+   {
+      /// <summary>
+      /// <para>A list of users that are editors for the specified broadcaster.</para>
+      /// <para>The list is empty if the broadcaster doesn’t have editors.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "data")]
+      public ChannelEditor[] Data { get; protected set; }
+   }
 }
