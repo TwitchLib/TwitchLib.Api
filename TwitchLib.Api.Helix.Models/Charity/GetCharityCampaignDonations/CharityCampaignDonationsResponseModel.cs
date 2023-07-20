43dc5b75011aa2d78c5df10759344a01a0029792
koishibuh <elysiagriffin@gmail.com>
2023-07-20 18:51:06 +0100

    - Updated summaries for GetCharityCampaign & GetCharityCampaignDonations

--- TwitchLib.Api.Helix.Models/Charity/GetCharityCampaignDonations/CharityCampaignDonationsResponseModel.cs
+++ TwitchLib.Api.Helix.Models/Charity/GetCharityCampaignDonations/CharityCampaignDonationsResponseModel.cs
@@ -1,46 +1,46 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TwitchLib.Api.Helix.Models.Charity.GetCharityCampaignDonations
 {
-    public class CharityCampaignDonationsResponseModel
-    {
-        /// <summary>
-        /// An ID that identifies the donation. The ID is unique across campaigns.
-        /// </summary>
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; protected set; }
+   /// <summary>
+   /// <para>Donations that users have made to the broadcaster's active charity campaign.</para>
+   /// </summary>
+   public class CharityCampaignDonationsResponseModel
+   {
+      /// <summary>
+      /// <para>An ID that identifies the donation. The ID is unique across campaigns.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "id")]
+      public string Id { get; protected set; }
 
-        /// <summary>
-        /// An ID that identifies the charity campaign that the donation applies to.
-        /// </summary>
-        [JsonProperty(PropertyName = "campaign_id")]
-        public string CampaignId { get; protected set; }
+      /// <summary>
+      /// <para>An ID that identifies the charity campaign that the donation applies to.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "campaign_id")]
+      public string CampaignId { get; protected set; }
 
-        /// <summary>
-        /// An ID that identifies a user that donated money to the campaign.
-        /// </summary>
-        [JsonProperty(PropertyName = "user_id")]
-        public string UserId { get; protected set; }
+      /// <summary>
+      /// <para>An ID that identifies a user that donated money to the campaign.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_id")]
+      public string UserId { get; protected set; }
 
-        /// <summary>
-        /// The user’s login name.
-        /// </summary>
-        [JsonProperty(PropertyName = "user_login")]
-        public string UserLogin { get; protected set; }
+      /// <summary>
+      /// <para>The user’s login name. (Name is lowercase)</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_login")]
+      public string UserLogin { get; protected set; }
 
-        /// <summary>
-        /// The user’s display name.
-        /// </summary>
-        [JsonProperty(PropertyName = "user_name")]
-        public string UserName { get; protected set; }
+      /// <summary>
+      /// <para>The user’s display name. (Name has capitalization)</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_name")]
+      public string UserName { get; protected set; }
 
-        /// <summary>
-        /// An object that contains the amount of money that the user donated.
-        /// </summary>
-        [JsonProperty(PropertyName = "amount")]
-        public Amount Amount { get; protected set; }
-    }
+      /// <summary>
+      /// <para>An object that contains the amount of money that the user donated.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "amount")]
+      public Amount Amount { get; protected set; }
+   }
 }
