14574362d630587d6fc1461ce4c3069822900999
koishibuh <elysiagriffin@gmail.com>
2023-07-20 15:49:17 +0100

    - Updated the GetChannelVIPs models

--- TwitchLib.Api.Helix.Models/Channels/GetChannelVIPs/ChannelVIPsResponseModel.cs
+++ TwitchLib.Api.Helix.Models/Channels/GetChannelVIPs/ChannelVIPsResponseModel.cs
@@ -2,22 +2,25 @@
 
 namespace TwitchLib.Api.Helix.Models.Channels.GetChannelVIPs
 {
-    public class ChannelVIPsResponseModel
-    {
-        /// <summary>
-        /// An ID that uniquely identifies the VIP user.
-        /// </summary>
-        [JsonProperty(PropertyName = "user_id")]
-        public string UserId { get; protected set; }
-        /// <summary>
-        /// The user’s display name.
-        /// </summary>
-        [JsonProperty(PropertyName = "user_name")]
-        public string UserName { get; protected set; }
-        /// <summary>
-        /// The user’s login name.
-        /// </summary>
-        [JsonProperty(PropertyName = "user_login")]
-        public string UserLogin { get; protected set; }
-    }
+   /// <summary>
+   /// <para>A user that is a VIP in a broadcaster's channel.</para>
+   /// </summary>
+   public class ChannelVIPsResponseModel
+   {
+      /// <summary>
+      /// <para>An ID that uniquely identifies the VIP user.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_id")]
+      public string UserId { get; protected set; }
+      /// <summary>
+      /// <para>The user’s display name. (Name has capitalization)</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_name")]
+      public string UserName { get; protected set; }
+      /// <summary>
+      /// <para>The user’s login name. (Name is lowercase)</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "user_login")]
+      public string UserLogin { get; protected set; }
+   }
 }
