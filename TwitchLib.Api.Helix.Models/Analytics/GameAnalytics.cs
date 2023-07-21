b5ff96d3241c513257fcfa37388991c75f94e143
koishibuh <elysiagriffin@gmail.com>
2023-07-21 21:17:32 +0100

    - Update summary for Analytics - Fixed summary bug in Anayltics.GetExtensionAnalyticsAsync() that copied the incorrect startedAt & endedAt information - Added summary for ExtensionAnalytics - Added summary for GetExtensionResponse - Added summary for GameAnalytics - Added summary for GetGameAnalyticsResponse - Added summary for DateRange

--- TwitchLib.Api.Helix.Models/Analytics/GameAnalytics.cs
+++ TwitchLib.Api.Helix.Models/Analytics/GameAnalytics.cs
@@ -3,15 +3,34 @@ using TwitchLib.Api.Helix.Models.Common;
 
 namespace TwitchLib.Api.Helix.Models.Analytics
 {
-    public class GameAnalytics
-    {
-        [JsonProperty(PropertyName = "game_id")]
-        public string GameId { get; protected set; }
-        [JsonProperty(PropertyName = "URL")]
-        public string Url { get; protected set; }
-        [JsonProperty(PropertyName = "type")]
-        public string Type { get; protected set; }
-        [JsonProperty(PropertyName = "date_range")]
-        public DateRange DateRange { get; protected set; }
-    }
+   /// <summary>
+   /// <para></para>
+   /// </summary>
+   public class GameAnalytics
+   {
+      /// <summary>
+      /// <para>An ID that identifies the game that the analytic report was generated for.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "game_id")]
+      public string GameId { get; protected set; }
+
+      /// <summary>
+      /// <para>The URL that you use to download the analytic report.</para>
+      /// <para><b>The URL is valid for 5 minutes.</b></para>
+      /// </summary>
+      [JsonProperty(PropertyName = "URL")]
+      public string Url { get; protected set; }
+
+      /// <summary>
+      /// <para>The type of analytic report.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "type")]
+      public string Type { get; protected set; }
+
+      /// <summary>
+      /// <para>The reporting period’s start and end dates.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "date_range")]
+      public DateRange DateRange { get; protected set; }
+   }
 }
--- TwitchLib.Api.Helix.Models/Analytics/GetExtensionAnalyticsResponse.cs
+++ TwitchLib.Api.Helix.Models/Analytics/GetExtensionAnalyticsResponse.cs
@@ -2,9 +2,19 @@
 
 namespace TwitchLib.Api.Helix.Models.Analytics
 {
-    public class GetExtensionAnalyticsResponse
-    {
-        [JsonProperty(PropertyName = "data")]
-        public ExtensionAnalytics[] Data { get; protected set; }
-    }
+   /// <summary>
+   /// <para>The response for GetExtensionAnalytics that gets a list of analytic reports for one or more extensions.</para>
+   /// <para>The response contains the URLs used to download the reports (CSV files) and the URLs are only valid for 5 minutes.</para>
+   /// </summary>
+   public class GetExtensionAnalyticsResponse
+   {
+      /// <summary>
+      /// <para>A list of analytic reports for the extensions.</para>
+      /// <para>The reports are returned in no particular order; however, the data within each report is in ascending order by date (newest first).</para>
+      /// <para>The report contains one row of data per day of the reporting window and only contains rows for the days that the extension was used.</para>
+      /// <para>The array is empty if there are no reports.</para>
+      /// </summary>
+      [JsonProperty(PropertyName = "data")]
+      public ExtensionAnalytics[] Data { get; protected set; }
+   }
 }
