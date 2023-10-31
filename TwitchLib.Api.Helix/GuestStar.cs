f8d0f4bafa4925e2fc72b965c5c048621e4a6832
Tobias 'Syzuna' Teske <syzuna@syzuna-programs.de>
2023-10-31 19:32:00 +0100

    Add GuestStar Api Section to Helix

---
+++ TwitchLib.Api.Helix/GuestStar.cs
@@ -0,0 +1,13 @@
+﻿using TwitchLib.Api.Core;
+using TwitchLib.Api.Core.Interfaces;
+
+namespace TwitchLib.Api.Helix;
+
+public class GuestStar : ApiBase
+{
+    public GuestStar(IApiSettings settings, IRateLimiter rateLimiter, IHttpCallHandler http) : base(settings, rateLimiter, http)
+    {
+    }
+    
+    
+}
