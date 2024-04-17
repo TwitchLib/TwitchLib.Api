a9ab808e6a033d3e478ae97f65bc4d1094a46e0a
swiftyspiffy <swiftyspiffy@gmail.com>
2024-04-17 22:09:56 +0545

    add remaining emote types

--- TwitchLib.Api.Helix.Models/Chat/Emotes/UserEmote.cs
+++ TwitchLib.Api.Helix.Models/Chat/Emotes/UserEmote.cs
@@ -13,13 +13,13 @@ public class UserEmote : Emote
     /// - channelpoints — An emote granted by using channel points.
     /// - rewards — An emote granted to the user through a special event.
     /// - hypetrain — An emote granted for participation in a Hype Train.
-    /// - An emote granted for linking an Amazon Prime account.
-    /// - An emote granted for having Twitch Turbo.
-    /// - Emoticons supported by Twitch.
-    /// - An emote accessible by everyone.
-    /// - Emotes related to Overwatch League 2019.
-    /// - Emotes granted by enabling two-factor authentication on an account.
-    /// - Emotes that were granted for only a limited time.
+    /// - prime — An emote granted for linking an Amazon Prime account.
+    /// - turbo — An emote granted for having Twitch Turbo.
+    /// - smilies — Emoticons supported by Twitch.
+    /// - globals — An emote accessible by everyone.
+    /// - owl2019 — Emotes related to Overwatch League 2019.
+    /// - twofactor — Emotes granted by enabling two-factor authentication on an account.
+    /// - limitedtime — Emotes that were granted for only a limited time.
     /// </summary>
     [JsonProperty("emote_type")]
     public string EmoteType { get; protected set; }
