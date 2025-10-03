1d3836dbf7754cdf3fcbcb44845cd09f215c5c0e
AoshiW <48525283+AoshiW@users.noreply.github.com>
2025-10-03 11:18:40 +0200

    [skip-ci] add `GetClipsDownloadAsync` (#422)
    add `GetClipsDownloadAsync` (#422)

---
+++ TwitchLib.Api.Helix.Models/Clips/GetClipsDownload/ClipDownload.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+#nullable enable
+
+namespace TwitchLib.Api.Helix.Models.Clips.GetClipsDownload;
+
+/// <summary>
+/// A clip that was created from a broadcaster's stream.
+/// </summary>
+public class ClipDownload
+{
+    /// <summary>
+    /// An ID that uniquely identifies the clip.
+    /// </summary>
+    [JsonProperty(PropertyName = "clip_id")]
+    public string ClipId { get; protected set; } = default!;
+
+    /// <summary>
+    /// The landscape URL to download the clip. This field is null if the URL is not available.
+    /// </summary>
+    [JsonProperty(PropertyName = "landscape_download_url")]
+    public string? LandscapeDownloadUrl { get; protected set; }
+
+    /// <summary>
+    /// The portrait URL to download the clip. This field is null if the URL is not available.
+    /// </summary>
+    [JsonProperty(PropertyName = "portrait_download_url")]
+    public string? PortraitDownloadUrl { get; protected set; }
+
+    /// <summary>
+    /// Indicates whether both download URLs are available.
+    /// </summary>
+
+#if NETCOREAPP
+    [MemberNotNullWhen(true, nameof(LandscapeDownloadUrl), nameof(PortraitDownloadUrl))]
+#endif
+    [JsonIgnore]
+    public bool IsDownloadUrlAvailable 
+        => !string.IsNullOrWhiteSpace(LandscapeDownloadUrl) && !string.IsNullOrWhiteSpace(PortraitDownloadUrl);
+}
---
+++ TwitchLib.Api.Helix.Models/Clips/GetClipsDownload/GetClipsDownloadResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitchLib.Api.Helix.Models.Clips.GetClipsDownload;
+
+/// <summary>
+/// Response for GetClipsDownload.
+/// </summary>
+public class GetClipsDownloadResponse
+{
+    /// <summary>
+    /// List of clips and their download URLs.
+    /// </summary>
+    [JsonProperty(PropertyName = "data")]
+    public ClipDownload[] Clips { get; protected set; }
+}
